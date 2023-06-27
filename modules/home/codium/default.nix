7117758f72882c8000d08e523f662a139fc82d6d
xenoxanite <xenoxanite@gmail.com>
2023-06-28 01:20:02 +0600

    code keybindings settings added

--- modules/home/codium/default.nix
+++ modules/home/codium/default.nix
@@ -9,16 +9,25 @@
             "window.menuBarVisibility": "hidden";
             "editor.mouseWheelZoom": "false";
         };
+        keybindings = {
+            [
+                {
+                  key = "ctrl+c";
+                  command = "editor.action.clipboardCopyAction";
+                  when = "textInputFocus";
+                }
+            ]
+        };
         extensions = with pkgs.vscode-extensions;[
             jnoortheen.nix-ide
             mskelton.one-dark-theme
             pkief.material-product-icons
         ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
             {
-                name = "min-theme";
-                publisher = "miguelsolorio";
-                version = "1.5.0";
-                sha256 = "DF/9OlWmjmnZNRBs2hk0qEWN38RcgacdVl9e75N8ZMY=";
+                # name = "min-theme";
+                # publisher = "miguelsolorio";
+                # version = "1.5.0";
+                # sha256 = "DF/9OlWmjmnZNRBs2hk0qEWN38RcgacdVl9e75N8ZMY=";
             }
         ];
     };
