924be61e7b9e8bb86c6d0004d53fe14cce537142
xenoxanite <xenoxanite@gmail.com>
2023-06-28 13:28:02 +0600

    Sound settings configured

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -51,59 +51,16 @@
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.xenoxanite = {
-    isNormalUser = true;
-    description = "Xenoxanite";
-    extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs; [kitty firefox neovim pavucontrol];
-  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   sound.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     inputs.alejandra.defaultPackage.${system}
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
   nix.settings.experimental-features = ["nix-command" "flakes"];
   programs.hyprland.enable = true;
   programs.sway.enable = true;
