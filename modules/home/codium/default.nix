ed5e7529fac7b51519a357118d349c02ba5f626a
xenoxanite <xenoxanite@gmail.com>
2023-06-29 00:28:07 +0600

    Vscodium configuration updated

--- modules/home/codium/default.nix
+++ modules/home/codium/default.nix
@@ -1,36 +1,44 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode;
-    #     userSettings = {
-    #       "workbench.colorTheme" = "One Dark";
-    #       "workbench.productIconTheme" = "material-product-icons";
-    #       "window.zoomLevel" = "0.3";
-    #       "window.menuBarVisibility" = "toggle";
-    #       "editor.mouseWheelZoom" = "false";
-    #       "editor.minimap.enabled" = "false";
-    #       "workbench.statusBar.visible" = "false";
-    #     };
-    #     keybindings = [
-    #       {
-    #         "key" = "ctrl+shift+i";
-    #         "command" = "editor.action.formatDocument";
-    #         "when" = "editorHasDocumentFormattingProvider && editorTextFocus && !editorReadonly && !inCompositeEditor";
-    #       }
-    #     ];
-    #     extensions = with pkgs.vscode-extensions;
-    #       [
-    #         jnoortheen.nix-ide
-    #         mskelton.one-dark-theme
-    #         pkief.material-product-icons
-    #       ]
-    #       ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    #         # {
-    #         # name = "min-theme";
-    #         # publisher = "miguelsolorio";
-    #         # version = "1.5.0";
-    #         # sha256 = "DF/9OlWmjmnZNRBs2hk0qEWN38RcgacdVl9e75N8ZMY=";
-    #         # }
-    #       ];
+    package = pkgs.vscodium;
+    userSettings = {
+      "workbench.colorTheme" = "Nord";
+      "workbench.iconTheme" = "a-file-icon-vscode";
+      "workbench.productIconTheme" = "a-file-icon-vscode-product-icon-theme";
+      "editor.fontFamily" = "JetbrainsMono nerd font";
+      "editor.fontSize" = 17;
+      "editor.lineHeight" = 2;
+      "window.menuBarVisibility" = "toggle";
+      "window.zoomLevel" = 0.3;
+      "workbench.statusBar.visible" = false;
+      "editor.minimap.enabled" = false;
+      "git.autofetch" = true;
+      "terminal.integrated.fontFamily" = "JetbrainsMono nerd font";
+      "terminal.integrated.fontSize" = 17;
+      "workbench.startupEditor" = "none";
+      "editor.mouseWheelZoom" = false;
+    };
+    keybindings = [
+      {
+        "key" = "ctrl+shift+i";
+        "command" = "editor.action.formatDocument";
+        "when" = "editorHasDocumentFormattingProvider && editorTextFocus && !editorReadonly && !inCompositeEditor";
+      }
+    ];
+    extensions = with pkgs.vscode-extensions;
+      [
+        kamadorueda.alejandra
+        arcticicestudio.nord-visual-studio-code
+        bbenoist.nix
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "a-file-icon-vscode";
+          publisher = "AtomMaterial";
+          version = "1.0.0";
+          sha256 = "nEmUPCpjiIqjvqWwWNsw44lLbVclRAa4Sa4VR9xkXUc=";
+        }
+      ];
   };
 }
