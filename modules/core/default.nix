c8b56205cbd7ba403b3798d2f95ffca081e0e2d4
xenoxanite <xenoxanite@gmail.com>
2023-06-28 13:03:09 +0600

    Wayland configuration added

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -1,20 +1,19 @@
-{ inputs
-, nixpkgs
-, self
-, ...
-}:
-let
+{
+  inputs,
+  nixpkgs,
+  self,
+  ...
+}: let
   system = "x86_64-linux";
   pkgs = import nixpkgs {
     inherit system;
     config.allowUnfree = true;
   };
   lib = nixpkgs.lib;
-in
-{
-   nixos = nixpkgs.lib.nixosSystem {
-    specialArgs = { inherit self inputs; };
-      system = "x86_64-linux";
-      modules = [(import ./../../hosts/nixos/configuration.nix)] ++ [(./home-manager.nix)];
-   };
+in {
+  nixos = nixpkgs.lib.nixosSystem {
+    specialArgs = {inherit self inputs;};
+    system = "x86_64-linux";
+    modules = [(import ./../../hosts/nixos/configuration.nix)] ++ [./home-manager.nix] ++ [./wayland.nix];
+  };
 }
---
+++ modules/core/wayland.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}: {
+  programs.hyprland.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+}
