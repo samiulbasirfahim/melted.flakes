812c96839cb6cb4f557c58498e4c26d748ade57c
xenoxanite <xenoxanite@gmail.com>
2023-07-05 22:47:25 +0600

    Updated boot and waydroid configuration

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -3,4 +3,9 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_lqx;
   boot.initrd.kernelModules = ["amdgpu"];
+  boot.kernelParams = ["quiet"];
+  boot.initrd.systemd.enable = true;
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "pixels";
+  environment.systemPackages = [pkgs.plymouth-theme];
 }
