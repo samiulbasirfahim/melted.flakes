be7890c028e7856eaea74d56e371d3a915bd9002
xenoxanite <xenoxanite@gmail.com>
2023-07-16 20:52:44 +0600

    Updaated waybar

--- modules/home/waybar/settings.nix
+++ modules/home/waybar/settings.nix
@@ -19,7 +19,6 @@
         "cpu"
         "disk"
         "network"
-        # "cava"
         "tray"
       ];
       "custom/launcher" = {
@@ -37,13 +36,6 @@
       "disk" = {
         "path" = "/home";
         "format" = "󰋊 {percentage_used}%";
-      };
-      "custom/wall" = {
-        "on-click" = "wallpaper_random";
-        "on-click-middle" = "default_wall";
-        "on-click-right" = "killall dynamic_wallpaper || dynamic_wallpaper &";
-        "format" = " ﴔ ";
-        "tooltip" = false;
       };
       "wlr/workspaces" = {
         "format" = "{icon}";
@@ -112,25 +104,6 @@
         "thermal-zone" = 2;
         "hwmon-path" = "/sys/class/hwmon/hwmon2/temp1_input";
         "format" = " {temperatureC}°C";
-      };
-      "cava" = {
-        "framerate" = 60;
-        "autosens" = 1;
-        "sensitivity" = 10;
-        "bars" = 6;
-        "lower_cutoff_freq" = 50;
-        "higher_cutoff_freq" = 10000;
-        "method" = "pulse";
-        "source" = "auto";
-        "stereo" = true;
-        "reverse" = false;
-        "bar_delimiter" = 0;
-        "monstercat" = false;
-        "waves" = false;
-        "noise_reduction" = 0.77;
-        "input_delay" = 2;
-        "format-icons" = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
-        "on-click-right" = "playerctl play-pause";
       };
       "tray" = {
         "icon-size" = 12;
