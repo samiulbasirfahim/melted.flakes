43c2adeae2c11b762bd14293d52193aeac059da6
xenoxanite <xenoxanite@gmail.com>
2023-07-19 13:04:53 +0600

    created theme reload config

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -1,3 +1,3 @@
 {...}: {
-  imports = [(import ./wallpaper-picker.nix)] ++ [(import ./random-wall.nix)] ++ [(import ./reload.nix)];
+  imports = [(import ./wallpaper-picker.nix)] ++ [(import ./random-wall.nix)] ++ [(import ./reload.nix)] + [(import ./reload-discord.nix)];
 }
--- modules/home/scripts/reload.nix
+++ modules/home/scripts/reload.nix
@@ -2,6 +2,7 @@
   reload = pkgs.writeShellScriptBin "reload" ''
     hyprctl reload &
     pkill waybar && setsid waybar &
+    reload-discord &
   '';
 in {
   home.packages = [reload];
