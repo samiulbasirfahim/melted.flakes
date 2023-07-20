c87fb4f1d939c3751a3843a66e364033ed7ec0a1
xenoxanite <xenoxanite@gmail.com>
2023-07-20 14:43:50 +0600

    Updated bootloader configuraiton

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -13,11 +13,5 @@
       systemd.enable = true;
     };
     kernelPackages = lib.mkDefault pkgs.linuxPackages_xanmod_latest;
-    kernelParams = [
-        "radeon.si_support=0"
-        "amdgpu.si_support=1"
-        "radeon.cik_support=0"
-        "amdgpu.cik_support=1"
-    ];
   };
 }
