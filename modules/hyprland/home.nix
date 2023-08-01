f64778bb5f4513c0f5097d9953b637a787976d73
xenoxanite <xenoxanite@gmail.com>
2023-08-02 01:25:30 +0600

    Updated hyprland

--- modules/hyprland/home.nix
+++ modules/hyprland/home.nix
@@ -57,7 +57,7 @@
           disable_hyprland_logo = true;
           always_follow_on_dnd = true;
           layers_hog_keyboard_focus = true;
-          animate_manual_resizes = true;
+          animate_manual_resizes = false;
           enable_swallow = true;
           focus_on_activate = true;
         };
