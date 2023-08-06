d2a4e7c37a2958926cb771afddca6b839f8c9def
xenoxanite <xenoxanite@gmail.com>
2023-08-06 23:40:06 +0600

    Wallpaper picker recursive menu open fixed

--- modules/home/scripts/wallpaper-picker.nix
+++ modules/home/scripts/wallpaper-picker.nix
@@ -6,8 +6,13 @@
     setWallpaper() {
        wallpaper_location="$(ls $wallpaper_folder/$temp_dir | $launcher)"
         if [[ -d $wallpaper_folder/$wallpaper_location ]]; then
-            temp_dir=$wallpaper_location
-            setWallpaper
+        temp_dir=""
+            if [ "$wallpaper_folder/$wallpaper_location" = "$wallpaper_folder/$temp_dir" ]; then
+                exit 0
+            else
+                temp_dir=$wallpaper_location
+                setWallpaper
+            fi
         elif [[ -f $wallpaper_folder/$temp_dir/$wallpaper_location ]]; then
             $wallpaper_daemon $wallpaper_folder/$temp_dir/$wallpaper_location &
             wal -i $wallpaper_folder/$temp_dir/$wallpaper_location  && reload &
